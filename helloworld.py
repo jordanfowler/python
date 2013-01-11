80afc86e308d126333a4c6cee5422c9f271584fb
jacob <jacob@ubuntu.ubuntu-domain>
2013-01-10 20:39:28 -0700

    executable

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python
 
 print "Hello, World!\n"
+
+choice = None
+while choice !="jordan":
+
+	choice = raw_input ("whats your name  ")
+		if choice=="jordan":
+			print "jordan is beautiful"
+		else:
+			print "huh"
